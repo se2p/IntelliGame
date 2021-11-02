3af8a53ecf21699000821098c4048e8ef70b9265
GitHub Action <action@github.com>
2021-11-02 21:39:41 +0000

    Template cleanup

--- src/main/kotlin/com/github/jonaslerchenberger/tesga/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/jonaslerchenberger/tesga/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.jonaslerchenberger.tesga.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.jonaslerchenberger.tesga.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/jonaslerchenberger/tesga/services/MyProjectService.kt
+++ src/main/kotlin/com/github/jonaslerchenberger/tesga/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.jonaslerchenberger.tesga.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.jonaslerchenberger.tesga.MyBundle
 
 class MyProjectService(project: Project) {
 
